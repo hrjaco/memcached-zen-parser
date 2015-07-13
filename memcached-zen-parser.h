8dc37dc035905be24bc4c964f2e87058177df301
Jiacheng Hong <jiacheng@pinterest.com>
2015-07-13 18:13:42 +0000

    Add initial parser files.

---
+++ memcached-zen-parser.h
@@ -0,0 +1,11 @@
+#ifndef MEMCACHED_ZEN_PARSER_H
+#define MEMCACHED_ZEN_PARSER_H
+#endif
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+    int get_sub_keys(const void* value, int value_len, char* req, int req_len, void** result);
+#ifdef __cplusplus
+};
+#endif
